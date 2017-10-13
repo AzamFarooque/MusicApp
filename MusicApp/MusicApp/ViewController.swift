059bd3f530860501185cf22bfd554269b90bdbdd
Farooque <azamfarooque69@gmail.com>
2017-10-13 22:00:48 +0530

    Project Setup

---
+++ MusicApp/MusicApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Farooque on 13/10/17.
+//  Copyright © 2017 riyaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
